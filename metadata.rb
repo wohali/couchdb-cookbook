99edae22b0f0bcb58d4c04f34176526da6d869ef
Sean OMeara <someara@opscode.com>
2013-12-11 19:27:30 -0500

    Version bump to v2.4.7

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ installed via some other method, either a backported package, or compiled
 directly from source. CouchDB is available on Red Hat-based systems through
 the EPEL Yum Repository.
 EOH
-version           "2.4.6"
+version           "2.4.7"
 depends           "erlang"
 depends           "yum", '< 3'
 recipe            "couchdb", "Installs and configures CouchDB package"
