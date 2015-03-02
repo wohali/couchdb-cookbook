291df7e14b4971f6629b34a91bebfdcfbaf51495
Peter Green <peter.green@aztek-native.com>
2015-03-02 16:09:38 +0000

    Log fatal error if unable to create DB.

--- providers/database.rb
+++ providers/database.rb
@@ -43,7 +43,7 @@ def create_database
   cmd = Mixlib::ShellOut.new(create_db_command)
   cmd.run_command
   response = JSON.parse(cmd.stdout)
-  response.include?("ok")
+  Chef::Log.fatal("Unable to create DB!") unless response.include?("ok")
 end
 
 def create_db_command
